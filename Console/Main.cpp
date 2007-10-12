a15a3e6e3605d30ab50eba3c8e94efeec0c61cfd
Admin <admin@chrisoldwood.com>
2007-10-12 21:45:15 +0000

    Initial version.

---
+++ Console/Main.cpp
@@ -0,0 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////
+//! \file   Main.cpp
+//! \brief  The console application entry point.
+//! \author Chris Oldwood
+
+#include "Common.hpp"
+#include <tchar.h>
+
+int _tmain(int /*nArgc*/, tchar* /*apszArgv*/[])
+{
+	return 0;
+}
